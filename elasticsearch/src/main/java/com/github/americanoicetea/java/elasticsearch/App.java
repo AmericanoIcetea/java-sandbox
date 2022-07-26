2f7276a1d7e02e90037a0c6a29aa883cf3af8b7a
AmericanoIceTea <chaiyanan.ser@gmail.com>
2022-07-27 01:50:54 +0700

    add outline sandbox

---
+++ elasticsearch/src/main/java/com/github/americanoicetea/java/elasticsearch/App.java
@@ -0,0 +1,13 @@
+package com.github.americanoicetea.java.elasticsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
